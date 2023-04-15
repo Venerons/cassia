623629d9b555edfbfef7158704f4ba87df2ba06a
Daniele Veneroni <Venerons@users.noreply.github.com>
2023-04-15 23:14:18 +0200

    Remove dist directory

--- dist/spa/assets/IndexPage.5fe556d5.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./QPage.c649a03e.js";import{_ as a,J as s,K as t,L as r,Z as c}from"./index.f737e56b.js";import"./render.019c9255.js";const o={},n=c("h1",null,"cassia",-1);function _(f,l){return s(),t(e,{class:"flex flex-center"},{default:r(()=>[n]),_:1})}var m=a(o,[["render",_]]);export{m as default};
