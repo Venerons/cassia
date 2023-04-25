3560c839ae1458d6ae552299574d79c2095974b6
Daniele Veneroni <Venerons@users.noreply.github.com>
2023-04-25 21:09:29 +0200

    Refactor aside menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -42,51 +42,48 @@ export default {
   data() {
     return {
       menu_items: [
-        // https://en.wikipedia.org/wiki/Substitution_cipher
-        { type: 'header', label: 'Monoalphabetic Ciphers' },
+        // Cryptography
+        { type: 'header', label: 'Cryptography' },
         { type: 'item', label: 'Albam', to: '/albam' },
         { type: 'item', label: 'Atbash', to: '/atbash' },
         { type: 'item', label: 'Augustus', to: '/augustus' },
         { type: 'item', label: 'Caesar', to: '/caesar' },
         { type: 'item', label: 'Carbonaro', to: '/carbonaro' },
-        //{ type: 'item', label: 'Pigpen', to: '/pigpen' }, // TODO
         { type: 'item', label: 'ROT13', to: '/rot13' },
         { type: 'item', label: 'Theban', to: '/theban' },
-        // custom substitution cipher
-        // custom shift cipher
 
+        // https://en.wikipedia.org/wiki/Substitution_cipher
         // https://en.wikipedia.org/wiki/Polyalphabetic_cipher
-        //{ type: 'separator' },
-        //{ type: 'header', label: 'Polyalphabetic Ciphers' },
-        // https://en.wikipedia.org/wiki/Alberti_cipher
-        // https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher
-        // https://en.wikipedia.org/wiki/Tabula_recta#Trithemius_cipher
-        // https://en.wikipedia.org/wiki/Enigma_machine
-        // Gronsfeld cipher
-        // https://en.wikipedia.org/wiki/Beaufort_cipher
-        // https://en.wikipedia.org/wiki/Autokey_cipher
-        // https://en.wikipedia.org/wiki/Running_key_cipher
-
         // https://en.wikipedia.org/wiki/Transposition_cipher
-        // ADFGVX
 
-        // Polygraphic Ciphers
-        // https://en.wikipedia.org/wiki/Playfair_cipher
-        // https://en.wikipedia.org/wiki/Bifid_cipher
-        // https://en.wikipedia.org/wiki/Four-square_cipher
-        // https://en.wikipedia.org/wiki/Trifid_cipher
-        // https://en.wikipedia.org/wiki/Hill_cipher
-        
-        /*
-        { type: 'item', label: 'XOR', to: '/' },
-        { type: 'item', label: 'Vernam One-Time Pad', to: '/' },
-        { type: 'item', label: 'Pizzini', to: '/' }, https://en.wikipedia.org/wiki/Pizzino
-        { type: 'item', label: 'Polibio', to: '/' },
-        { type: 'item', label: 'Pollux', to: '/' },
-        { type: 'item', label: 'VIC', to: '/' },
-        { type: 'item', label: 'Random Break', to: '/' },
-        { type: 'item', label: 'Transposition', to: '/' },
-        */
+        //{ type: 'item', label: 'Pigpen', to: '/pigpen' },
+        //{ type: 'item', label: 'Custom Substitution', to: '/customsubstitution' },
+        //{ type: 'item', label: 'Custom Shift', to: '/customshift' },
+        //{ type: 'item', label: 'Alberti', to: '/alberti' }, https://en.wikipedia.org/wiki/Alberti_cipher
+        //{ type: 'item', label: 'Vigenère', to: '/vegenere' }, https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher
+        //{ type: 'item', label: 'Trithemius', to: '/trithemius' }, https://en.wikipedia.org/wiki/Tabula_recta#Trithemius_cipher
+        //{ type: 'item', label: 'Enigma', to: '/enigma' }, https://en.wikipedia.org/wiki/Enigma_machine
+        //{ type: 'item', label: 'Gronsfeld', to: '/gronsfeld' },
+        //{ type: 'item', label: 'Beaufort', to: '/beaufort' }, https://en.wikipedia.org/wiki/Beaufort_cipher
+        //{ type: 'item', label: 'Autokey', to: '/autokey' }, https://en.wikipedia.org/wiki/Autokey_cipher
+        //{ type: 'item', label: 'Running Key', to: '/runningkey' }, https://en.wikipedia.org/wiki/Running_key_cipher
+        //{ type: 'item', label: 'ADFGVX', to: '/adfgvx' },
+        //{ type: 'item', label: 'XOR', to: '/xor' },
+        //{ type: 'item', label: 'Vernam One-Time Pad', to: '/vernam' },
+        //{ type: 'item', label: 'Pizzini', to: '/pizzini' }, https://en.wikipedia.org/wiki/Pizzino
+        //{ type: 'item', label: 'Polibio', to: '/polibio' },
+        //{ type: 'item', label: 'Pollux', to: '/pollux' },
+        //{ type: 'item', label: 'VIC', to: '/vic' },
+        //{ type: 'item', label: 'Playfair', to: '/playfair' }, https://en.wikipedia.org/wiki/Playfair_cipher
+        //{ type: 'item', label: 'Bifid', to: '/bifid' }, https://en.wikipedia.org/wiki/Bifid_cipher
+        //{ type: 'item', label: 'Four-Square', to: '/foursquare' }, https://en.wikipedia.org/wiki/Four-square_cipher
+        //{ type: 'item', label: 'Trifid', to: '/trifid' }, https://en.wikipedia.org/wiki/Trifid_cipher
+        //{ type: 'item', label: 'Hill', to: '/hill' }, https://en.wikipedia.org/wiki/Hill_cipher
+
+        // Steganography
+        { type: 'header', label: 'Steganography' },
+        //{ type: 'item', label: 'Random Break', to: '/randombreak' },
+        //{ type: 'item', label: 'Transposition', to: '/transposition' },
 
         // Encodings
         { type: 'separator' },
